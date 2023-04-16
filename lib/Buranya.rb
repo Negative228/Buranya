f94b43ed1d74a81e4a484346adf88564a9203423
Askobarus <salnikov.salnikov2010@yandex.ru>
2023-04-16 15:54:59 +0300

    client overthrown

--- lib/Buranya.rb
+++ lib/Buranya.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
-require_relative "Buranya/version"
+require "httparty"
+require "Buranya/version"
+require "Buranya/client"
 
 module Buranya
   class Error < StandardError; end
   # Your code goes here...
+
+  def cat_pic(breed)
+    response = HTTParty.get("https://api.api-ninjas.com/v1/cats?name=#{breed}")
+    puts response.body if response.code == 200
+
+  end
 end
