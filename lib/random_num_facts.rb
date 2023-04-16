e55db3543c768fe5dbf1c9f75155a1af72aceea0
Yasha-sha <2012_slava@bk.ru>
2023-04-16 22:47:24 +0300

    'install'

--- lib/random_num_facts.rb
+++ lib/random_num_facts.rb
@@ -1,20 +1,16 @@
 # frozen_string_literal: true
+require 'json'
 require 'nokogiri'
+require 'open-uri'
 
-module GrooveHQ
-  class Client
-
-    module Rand_Num
 
       def facts(num)
-        url = 'http://numbersapi.com/' + to_str(num)
-        tt = open(url)
-        f = self.class.get("http://numbersapi.com/" + to_str(num), { query: options })
-        puts f.parsed_response["tickets"]
+        url = 'http://numbersapi.com/' + num.to_s
+        html = open(url)
+
+        puts JSON.pretty_generate(url)
+        #f = self.class.get("http://numbersapi.com/" + to_str(num), { query: options })
+        #puts f.parsed_response["tickets"]
       end
 
-    end
-
-  end
-end
-
+facts(15)
