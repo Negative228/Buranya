9d14d29ce9ad3149ddcb833a955c82211f3e0d10
Askobarus <salnikov.salnikov2010@yandex.ru>
2023-04-16 16:07:29 +0300

    initial cats request done

--- lib/Buranya.rb
+++ lib/Buranya.rb
@@ -9,8 +9,11 @@ module Buranya
   # Your code goes here...
 
   def cat_pic(breed)
-    response = HTTParty.get("https://api.api-ninjas.com/v1/cats?name=#{breed}")
-    puts response.body if response.code == 200
-
+    response = HTTParty.get("https://api.api-ninjas.com/v1/cats?name=#{breed}" , headers: {'X-Api-Key': 'wCwPBOjfVh4Sd6+wk5qlag==iTrhbDSMz9f3ppXh'})
+    if response.code == 200
+      puts response.body
+    else
+      puts 'Error:', response.code, response.body
+    end
   end
 end
