f65c8a8f3e0be9310b10375ceb2226b7ef5a263c
Askobarus <salnikov.salnikov2010@yandex.ru>
2023-04-16 17:24:26 +0300

    link to cat image response
    finalised

--- lib/Buranya.rb
+++ lib/Buranya.rb
@@ -1,19 +1,25 @@
 # frozen_string_literal: true
 
-require "httparty"
+
 require "Buranya/version"
-require "Buranya/client"
+
 
 module Buranya
   class Error < StandardError; end
   # Your code goes here...
 
-  def cat_pic(breed)
-    response = HTTParty.get("https://api.api-ninjas.com/v1/cats?name=#{breed}" , headers: {'X-Api-Key': 'wCwPBOjfVh4Sd6+wk5qlag==iTrhbDSMz9f3ppXh'})
-    if response.code == 200
-      puts response.body
-    else
-      puts 'Error:', response.code, response.body
+  module MemMethods
+    def cat_pic(breed)
+      response = HTTParty.get("https://api.api-ninjas.com/v1/cats?name=#{breed}",
+                              headers: {'X-Api-Key': 'wCwPBOjfVh4Sd6+wk5qlag==iTrhbDSMz9f3ppXh'})
+
+      if response.code == 200
+        puts JSON.parse(response.body)[0]["image_link"]
+      else
+        puts 'Error:', response.code, response.body
+      end
     end
   end
 end
+
+
